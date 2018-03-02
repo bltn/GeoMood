a00aa8bdd034b194f5d15188458663f06d667d42
Ben Lawton <bltn@Bens-MacBook-Air.local>
2018-03-02 15:15:38 +0000

    users can see tweets for a given topic rendered over a map, colour-coded by sentiment value

--- app/controllers/MapController.java
+++ app/controllers/MapController.java
@@ -1,9 +1,20 @@
 package controllers;
 
+import models.Tweet;
 import play.mvc.Controller;
 import play.mvc.Result;
+import repositories.TweetRepository;
+import repositories.TweetRepositoryFactory;
 import views.html.map_visualisation;
 
+import java.util.List;
+
 public class MapController extends Controller {
-    public Result renderMapVisualisation() { return ok(map_visualisation.render()); }
+
+    public Result visualiseTopic() {
+        String topic = request().getQueryString("topic");
+        TweetRepository repo = TweetRepositoryFactory.getTweetRepository("dev");
+        List<Tweet> sampleTweets = repo.findTweetsWithTopic(topic);
+        return ok(map_visualisation.render(sampleTweets));
+    }
 }
---
+++ public/javascripts/map.js
@@ -0,0 +1,38 @@
+function initMap() {
+    // Start the map focused on Birmingham
+    var birmingham = {lat: 52.4862, lng: 1.8904};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: birmingham
+    });
+
+    // fetch icons to represent negative, neutral and positive sentiment
+    var negative_icon = "https://maps.gstatic.com/mapfiles/ms2/micons/red-dot.png";
+    var neutral_icon = "https://maps.gstatic.com/mapfiles/ms2/micons/blue-dot.png";
+    var positive_icon = "https://maps.gstatic.com/mapfiles/ms2/micons/green-dot.png";
+
+    var tweets = document.getElementsByClassName("tweet");
+
+    // loop over all tweet coordinates and overlay them onto the map
+    for(var i=0; i < tweets.length; i++) {
+        var tweetLatitude = Number(tweets[i].dataset.lat);
+        var tweetLongitude = Number(tweets[i].dataset.lng);
+        var sentiment = Number(tweets[i].dataset.sentiment);
+        var tweetIcon;
+
+        // assign icon based on sentiment value
+        if (sentiment <= 1) {
+            tweetIcon = negative_icon;
+        } else if (sentiment === 2) {
+            tweetIcon = neutral_icon;
+        } else if (sentiment > 2) {
+            tweetIcon = positive_icon;
+        }
+
+        new google.maps.Marker({
+            position: {lat: tweetLatitude, lng: tweetLongitude},
+            map: map,
+            icon: tweetIcon
+        });
+    }
+}
